ab980f476b8723ad3451931580b3b1f574ea198c
mfariza2111 <farizamuhammad310@gmail.com>
2021-12-06 05:35:12 +0700

    Add project files.

---
+++ ServiceReservasi/CompositeType.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceReservasi
+{
+    public class CompositeType
+    {
+        public bool BoolValue { get; internal set; }
+        public string StringValue { get; internal set; }
+    }
+}
---
+++ ServiceReservasi/IService1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using static ServiceReservasi.Service1;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Pemesanan
+    {
+        [DataMember]
+        public string IDPemesanan { get; set; }
+        [DataMember]
+        public string NamaCustomer { get; set; } //method
+        [DataMember]
+        public string NoTelpon { get; set; }
+        [DataMember]
+        public int JumlahPemesanan { get; set; }
+        [DataMember]
+        public string IDLokasi { get; set; }
+    }
+    public class DetailLokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variabel dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeksripsiFull { get; set; }
+        [DataMember]
+        public int Kuota { get; set; }
+    }
+    public class CekLokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variabel dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeksripsiSingkat { get; set; }
+    }
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string Pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);
+        [OperationContract]
+        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        [OperationContract]
+        string deletePemesanan(string IDPemesanan);
+        [OperationContract]
+        List<CekLokasi> ReviewLokasi();
+        [OperationContract]
+        List<DetailLokasi> DetailLokasi(); //menampilkan detail lokasi
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi1.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
